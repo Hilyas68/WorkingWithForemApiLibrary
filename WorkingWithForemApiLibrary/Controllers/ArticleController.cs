fb721f481372c190e08cfd230a65e700c98810c1
Hassan Liasu <htope68@gmail.com>
2020-08-23 17:09:27 +0100

    Initial

---
+++ WorkingWithForemApiLibrary/Controllers/ArticleController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Forem.Api;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WorkingWithForemApiLibrary.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+        private readonly IArticlesService _articlesService;
+
+        public ArticleController(IArticlesService articlesService)
+        {
+            _articlesService = articlesService;
+        }
+
+
+
+        [HttpGet("getAllArticles")]
+        public async Task<IActionResult> GetAllArticles(int page, int itemPerPage)
+        {
+            var articlesService = new ArticlesService(new Uri("https://dev.to/"), new HttpClient());
+            return Ok(await articlesService.GetArticlesAsync(page, itemPerPage));
+        }
+    }
+}
